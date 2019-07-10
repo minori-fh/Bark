20ef771d7cffadad11d210643bbf22cd056e3bdd
Adam <lehreradam@yahoo.com>
2019-07-10 12:45:48 -0700

    starting up server file

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+var path= require("path")
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.listen(PORT, function(){
+     console.log("App listening on PORT " + PORT)
+})
+
+require("./route/html-route.js")(app);
+
+var my 
