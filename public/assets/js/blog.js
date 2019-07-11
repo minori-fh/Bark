04e7088169575761ffe8e64d0f134f65c1239bbb
Minori Hashimoto <minori.fh@gmail.com>
2019-07-11 13:46:34 -0700

    edit createNewPost function

--- public/assets/js/blog.js
+++ public/assets/js/blog.js
@@ -31,28 +31,36 @@ function initializeRows(){
 };
 
 function createNewRow(post){
+    //CREATE NEW post card
     var newPostCard = $("<div>");
     newPostCard.addClass("card");
+    //CREATE NEW post card heading
     var newPostCardHeading = $("<div>");
     newPostCardHeading.addClass("card-header");
+    //CREATE NEW upvote button
     var upvoteBtn = $("<button>");
     upvoteBtn.text("Upvote");
     upvoteBtn.addClass("upvote btn btn-primary");
-
+    //CREATE NEW post title
     var newPostTitle = $("<h2>");
+    //CREATE NEW post date
     var newPostDate = $("<small>");
+    //CREATE NEW post category
     var newPostCategory = $("<h5>");
-    newPostCategory.text(post.category);
-
+    newPostCategory.text(post.category); //grab category from post
+    //CREATE NEW card body
     var newPostCardBody = $("<div>");
     newPostCardBody.addClass("card-body");
+    //CREATE NEW post body
     var newPostBody = $("<p>");
-    newPostTitle.text(post.title + " ");
-    newPostBody.text(post.body);
 
-    // need to make fomatted date with moments
-    newPostDate.text(post.createdAt);
+
+    newPostTitle.text(post.title + " "); //grab title from post
+    newPostBody.text(post.body); //grab body from post
+        // need to make fomatted date with moments
+    newPostDate.text(post.createdAt); //grab created at from post
     newPostTitle.append(newPostDate);
+
 
     newPostCardHeading.append(upvoteBtn);
     newPostCardHeading.append(newPostTitle);
@@ -62,8 +70,6 @@ function createNewRow(post){
     newPostCard.append(newPostCardBody);
     newPostCard.data("post", post);
     return newPostCard;
-
-
 }
 
 getPosts();
