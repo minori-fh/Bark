0d93e3291b3c0f85ebc09bba946df9e59f8ab039
Minori Hashimoto <minori.fh@gmail.com>
2019-07-12 13:03:19 -0700

    edit html routes to include login page based on user authentication

--- routes/view/html-routes.js
+++ routes/view/html-routes.js
@@ -1,9 +1,33 @@
+var router = require("express").Router();
 var path = require("path");
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/login.html"))
-    })
+//     app.get("/", function (req, res) {
+//         res.sendFile(path.join(__dirname, "./public/login.html"))
+//     })
 
-}
+// }
+// ---------------------------
+router.get("/", function(req, res){
+    var auth = req.isAuthenticated();
+    console.log("hi hi")
+    if(auth) {
+        res.sendFile(path.join(__dirname, "/../../public/index.html"));
+    } else {
+        res.sendFile(path.join(__dirname, "/../../public/login.html"));
+    }
+
+});
+
+router.get('/index', function(req, res){
+    var auth = req.isAuthenticated();
+    console.log("hi")
+    if(auth) {
+        res.sendFile(path.join(__dirname, "/../../public/index.html"));
+    } else {
+        res.redirect("/");
+    }
+})
+
+module.exports = router;
