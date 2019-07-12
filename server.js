2a5074d9871b8a950608f0cb2226dd5b55f8a6bf
Minori Hashimoto <minori.fh@gmail.com>
2019-07-12 12:55:55 -0700

    edit server.js to require new packages

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 var express = require("express");
+var passport = require("passport");
+var session = require("express-session");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var routes = require("./routes")
 var db = require("./models");
 
+// Configuration =========
+require("./config/passport")(passport);
+
+// Middleware ============
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+app.use(session({
+  key: 'user_sid',
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+      expires: 10800000, // 3 HRS
+      httpOnly: false
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.use(routes);
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
+// Launch server ==============
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
