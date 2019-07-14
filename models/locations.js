4f275c44e5f7a42f13bc0ab269c997a867c200b8
Jake Dudum <jakedudum@gmail.com>
2019-07-13 19:31:35 -0700

    Refactored code to use router instead of app and added controllers for location and post as well as gave them routes in index.js inside of router directory

--- models/locations.js
+++ models/locations.js
@@ -1,7 +1,11 @@
+var Sequelize = require("sequelize");
+
 module.exports = function (sequelize, DataTypes) {
   var Location = sequelize.define("Location", {
     area: DataTypes.STRING
-  });
+  }, {
+      timestamps: false
+    });
 
   Location.associate = function (models) {
     Location.hasMany(models.Post, {
